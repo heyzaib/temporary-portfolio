fbf77ad4ad121f9257879163092566d842f756c2
Zaib <95955709+zaibcode@users.noreply.github.com>
2022-02-10 13:30:01 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Zaib.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © Zaib
       <style jsx>{`
         a {
           float: right;
